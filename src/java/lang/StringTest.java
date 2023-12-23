d768cb73eb0be69dd4625b99917297820ce51bbb
hizemchaima <hizemchayma@gmail.com>
2023-12-23 19:17:06 +0100

    first commit

---
+++ src/java/lang/StringTest.java
@@ -0,0 +1,14 @@
+package java.lang;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
